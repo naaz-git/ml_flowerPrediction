351c97d9cc16c9b6c2024c7a4ead969f4f51a519
Naaz <naaz@MacBookAir.lan>
2024-12-20 00:44:24 -0500

    IRIS flower prediction using streamlit

---
+++ flower_prediction_webapp.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Simple Iris Flower Prediction App
+
+This app predicts the **Iris flower** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+# Function to collect user input
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal length (cm)': sepal_length,
+            'sepal width (cm)': sepal_width,
+            'petal length (cm)': petal_length,
+            'petal width (cm)': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+# Load Iris dataset
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data, columns=iris.feature_names)  # Use the feature names from the dataset
+Y = iris.target
+
+# Train RandomForestClassifier
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+# Make predictions
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction[0]])  # Display the predicted class
+# st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
